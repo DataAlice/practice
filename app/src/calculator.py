90da5b4ce040a3f3da971e075c00f314e8935fad
Alice <jin.h.yang2013@gmail.com>
2022-07-17 20:06:14 -0700

    add new files

---
+++ app/src/calculator.py
@@ -0,0 +1,23 @@
+class Calculator():
+
+    def add(self,num1,num2):
+        return num1+num2
+
+    def substract(self, num1, num2):
+        return num1-num2
+
+#    def multiply(self,num1,num2):
+#	return num1*num2
+
+ #   def devide(self,num1,num2):
+#	if num2 == 0:
+#	    return 0
+#	else:
+#	    return num1/num2
+ 
+if __name__ == '__main__':	
+    calculator = Calculator() 
+    print(calculator.add(1,3))
+ #   print(calculator.devide(10,0)) 
+  #  print(calculator.devide(10,2)) 
+
